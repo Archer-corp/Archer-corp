fcc4986f43845976902f9aa1807ebb6047a7d33f
IS006620 <40972618+IS006620@users.noreply.github.com>
2018-07-20 03:32:34 +0900

    App.jsxはより大きなスケールのページ管理にして、今までApp似合ったものはMainPageへ

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
-﻿import logo from './logo.svg';
-import './App.css';
-
-import React from 'react';
+﻿import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import ReactDOM from 'react-dom';
 
-import MypageIcon from '@material-ui/icons/Person';
-import RankingIcon from '@material-ui/icons/Star';
-import FolderIcon from '@material-ui/icons/Favorite';
-import HomeIcon from '@material-ui/icons/Home';
-
-import { createMuiTheme } from '@material-ui/core/styles';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-//import { theme } from '../../assets/theme';
+//別のソースをインポート
+import './App.css';
+import MainPage from './mainPage.jsx'
 
 import purple from '@material-ui/core/colors/purple';
 import orange from '@material-ui/core/colors/orange';
 import Button from '@material-ui/core/Button';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
-//別のソースをインポート
-import HomePage from './home.jsx';
-import RankingPage from './ranking.jsx';
-import MyPage from './myPage.jsx';
-import ContentPage from './content.jsx';
+import {
+    BrowserRouter,
+    Route
+} from "react-router-dom";
 
 
 export const myTheme = createMuiTheme({
@@ -38,51 +30,21 @@ export const myTheme = createMuiTheme({
 
 //CSS
 const styles = {
-    root: {
-        position:'fixed',
-        width: window.innerWidth,
-        bottom: 0,
-        color:'red',
-    },
+
 };
 
 class Archer extends React.Component {
-    state = {
-
-        value: 0,
-    };
-
-    handleChange = (event, value) => {
-        this.setState({ value });
-    };
 
     //index.htmlの中身的な
     render() {
-        const { classes } = this.props;
-        const { value } = this.state;
-
         return (
-            <MuiThemeProvider theme={myTheme}>
-            <BottomNavigation
-                value={value}
-                onChange={this.handleChange}
-                showLabels
-                className={classes.root}
-            >
-                    <BottomNavigationAction label="Home" icon={<HomeIcon />}/> 
-                <BottomNavigationAction label="Content" icon={<FolderIcon />} />
-                <BottomNavigationAction label="Ranking" icon={<RankingIcon />} />
-                <BottomNavigationAction label="My page" icon={<MypageIcon />} />
-                </BottomNavigation>
+            <BrowserRouter>
+                <MuiThemeProvider theme={myTheme}>
 
+                    <Route exact path="/" component={MainPage} />
 
-                {value === 0 && <HomePage /> }
-
-                {value === 1 && <ContentPage />}
-                {value === 2 && <RankingPage/>}
-            {value === 3 && <MyPage/>}
-                
-            </MuiThemeProvider>
+                </MuiThemeProvider>
+            </BrowserRouter>
         );
     }
 }
---
+++ src/mainPage.jsx
@@ -0,0 +1,72 @@
+﻿import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+import MypageIcon from '@material-ui/icons/Person';
+import RankingIcon from '@material-ui/icons/Star';
+import FolderIcon from '@material-ui/icons/Favorite';
+import HomeIcon from '@material-ui/icons/Home';
+
+import HomePage from './home.jsx';
+import RankingPage from './ranking.jsx';
+import MyPage from './myPage.jsx';
+import ContentPage from './content.jsx';
+
+//UIパーツ
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import Divider from '@material-ui/core/Divider';
+
+//CSS
+var mainPageCSS = {
+    bottomTab: {
+        position: 'fixed',
+        width: window.innerWidth,
+        bottom: 0,
+    },
+
+};
+
+class MainPage extends React.Component {
+    //最初は一番左のタブ
+    state = {
+        value: 0,
+    };
+
+    //下のタブで切り替えられた
+    handleChange = (event, value) => {
+        this.setState({ value });
+    };
+
+    //index.htmlの中身的な
+    render() {
+        const { value } = this.state;
+
+        return (
+            <div>
+                <div style={mainPageCSS.bottomTab}>
+                    <Divider />
+                    <Divider />
+                    <Divider />
+                    <BottomNavigation
+                        value={value}
+                        onChange={this.handleChange}
+                        showLabels
+                    >
+                        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+                        <BottomNavigationAction label="Content" icon={<FolderIcon />} />
+                        <BottomNavigationAction label="Ranking" icon={<RankingIcon />} />
+                        <BottomNavigationAction label="My page" icon={<MypageIcon />} />
+                    </BottomNavigation>
+                </div>
+
+                {value === 0 && <HomePage />}
+                {value === 1 && <ContentPage />}
+                {value === 2 && <RankingPage />}
+                {value === 3 && <MyPage />}
+            </div>
+        );
+    }
+}
+
+export default (MainPage);
