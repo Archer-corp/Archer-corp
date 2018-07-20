42d6b09fed672e96342b4e18ab62114628a20d31
IS006620 <40972618+IS006620@users.noreply.github.com>
2018-07-20 16:36:34 +0900

    ログインページの見た目調整

--- src/loginPage.jsx
+++ src/loginPage.jsx
@@ -1,6 +1,7 @@
 ﻿import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { withRouter } from 'react-router'
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -9,39 +10,70 @@ import Button from '@material-ui/core/Button';
 //CSS
 var loginPageCSS = {
 
+    text: {
+        width:'4em',
+        margin:'auto',
+    },
+    input: {
+        display: 'flex',
+        width: '80%',
+        margin: 'auto',
+    },
+    button: {
+        width:'30%',
+        left:window.innerWidth*0.6  ,
+    },
 };
 
 class LoginPage extends React.Component {
+    state = {
+        email: '',
+        password:'',
+    };
 
-    onClickedLogin=function(){
+    onClickedLogin = () => {
         console.log("LOGIN!");
+        this.props.history.push("/");
+    }
+
+    onChangeEmail = event => {
+        this.setState({ email: event.target.value });
+       // console.log(event.target.value);
+    }
+
+    onChangePassword = event => {
+        this.setState({ password: event.target.value });
+        //console.log(this.state.password);
     }
 
     //index.htmlの中身的な
     render() {
         return (
             <div>
-                <p>ログイン</p>
+                <p style={loginPageCSS.text}>ログイン</p>
 
                 <TextField
                     id="emailaddress-input"
                     label="email"
                     type="email"
                     autoComplete="current-email"
-                    margin="normal"
-                />
-
+                    onChange={this.onChangeEmail}
+                    style={loginPageCSS.input}
+               />
+                <p/>
                 <TextField
                     id="password-input"
                     label="Password"
                     type="password"
                     autoComplete="current-password"
-                    margin="normal"
+                    onChange={this.onChangePassword}
+                    style={loginPageCSS.input}
                 />
-                <Button color="secondary" variant="contained" onClick={this.onClickedLogin}>Login</Button>
+                <p/>
+                <Button color="secondary" variant="contained" onClick={this.onClickedLogin} style={loginPageCSS.button}>Login</Button>
             </div>
         );
     }
 }
 
-export default (LoginPage);
+export default withRouter(LoginPage);
